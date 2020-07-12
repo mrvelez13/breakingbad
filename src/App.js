c95c7487287297077c300983dd530b4255b63f0f
Johnatan Vélez <mrvelez13@gmail.com>
2020-07-12 17:16:21 -0500

    braking bad quotes

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import Quote from "./components/Quote";
+
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  
+  :hover{
+  cursor:pointer;
+  background-size: 400px;
+  }
+`;
+
+function App(){
+
+    const [quote, saveQuote] = useState({});
+
+    const callApi = async () => {
+        const result = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+        const quote = await result.json();
+        saveQuote(quote[0]);
+    }
+
+    useEffect(() => {
+        callApi();
+    }, [])
+
+    return (
+        <Container>
+            <Quote
+                quote={quote}
+            ></Quote>
+            <Button
+                onClick={callApi}
+            >Get Quote</Button>
+        </Container>
+    );
+}
+
+export default App;
